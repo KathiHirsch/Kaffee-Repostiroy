30b481876bd389ffbb6c12651607916cac6f46da
Kathi <kat.hirsch@hakvb.at>
2026-04-08 08:55:35 +0200

    Das Script wurde geändert

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function begruessungAnzeigen() {
     const ausgabe = document.getElementByld("ausgabe");
-    ausgabe.textContent = "Heute gibt es frischen Apfelstrudel und Cappuccino.";
+    ausgabe.textContent = "Heute gibt es frischen Apfelstrudel und CafeLatte";
 }
